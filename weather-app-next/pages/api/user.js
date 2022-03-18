75ec1be1d2269f39664088aab851195775c923ee
beny996 <94617004+beny996@users.noreply.github.com>
2022-03-18 21:47:23 +0100

    Pocetak nextjs dela weather aplikacije

---
+++ weather-app-next/pages/api/user.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default function handler(req, res) {
+  console.log(req.body);
+}
--- weather-app-next/pages/api/weather.js
+++ weather-app-next/pages/api/weather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=f0dde32af7b09f551fd40f088122139a`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.body}&units=metric&appid=f0dde32af7b09f551fd40f088122139a`;
   const resp = await axios.get(url);
 
   res.status(200).json(resp.data);
